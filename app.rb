f9ef85a0750c7d870933d3984c971409a293f392
conner fortner <fortner.conner@yahoo.com>
2020-12-02 21:11:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,6 +3,18 @@ class App < Sinatra::Base
 	get '/' do
 		erb :index
 	end
+	
+	get '/' do
+	  erb :hello
+	end
+	
+	get '/' do
+	  erb :goodbye
+	end
+	
+	get '/' do
+	  erb :datetime_select
+	end  
 
 
 end
